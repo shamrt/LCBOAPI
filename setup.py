a21b79bd23378f1dc8da238a500d938611ec2dbc
Shane Martin <dev.sh@nemart.in>
2015-07-12 11:02:31 -0400

    Add setup files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+long_description = open('README.md').read()
+
+setup(
+  name = 'lcboapi',
+  packages = ['lcboapi'],
+  version = '0.1',
+  description = 'Python wrapper for the unofficial LCBO API',
+  long_description = long_description,
+  author = 'Shane Martin',
+  author_email = 'dev.sh@nemart.in',
+  license='MIT License',
+  url = 'https://github.com/shamrt/LCBOAPI',
+  keywords = ['api', 'lcbo'],
+  platforms = ['any'],
+  classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+    ],
+)
